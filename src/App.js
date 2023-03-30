083d0ea725a1dde1c50f7bef62fe3c025cf8f2b0
tie-lar <tyleroliverrobert@gmail.com>
2023-03-30 18:24:27 +0100

    updated!

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Navbar} from './components';
+import { Footer} from './components';
+import './App.css';
+
+const App = () => {
+  return (
+      <div className='App'> 
+          <div>
+              <Navbar />
+          </div>
+
+      </div>
+  );
+};
+
+export default App
