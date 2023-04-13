e55ee2f125971a6d5b62c7b8f4363781671e3309
Dirley Jordan <dirleyjordan@gmail.com>
2023-04-13 19:16:29 +0100

    Alter bot appearance to fit better with the site theme - Changed the bot's name and the box's colour scheme - Implemented cringe first message

--- src/components/chatWindow/config.js
+++ src/components/chatWindow/config.js
@@ -1,7 +1,19 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 
+const botName = 'OntrackBot';
+
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)],
+  botName: botName,
+  customStyles: {
+    botMessageBox: { backgroundColor: 'purple', },
+    chatButton: { backgroundColor: 'purple', },
+  },
+  
+  initialMessages: [
+    createChatBotMessage(`Hi! I'm ${botName}.`),
+    createChatBotMessage(`My goal is to help keep you happy, healthy and ontrack.`),
+    createChatBotMessage(`If there is anything I can help you with, just let me know!`),
+  ],
 };
 
 export default config;
