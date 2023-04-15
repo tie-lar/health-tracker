c403f0fcae69d713120b301cfc24316c72350c28
Dirley Jordan <dirleyjordan@gmail.com>
2023-04-15 20:43:18 +0100

    Add signup help - Added help message for "sign" and "signup" - Changed tab width to four spaces as God intended

--- src/components/chatWindow/ActionProvider.jsx
+++ src/components/chatWindow/ActionProvider.jsx
@@ -1,54 +1,78 @@
 import React from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
-  const handleLogin = () => {
-    const messageArr = [
-      createChatBotMessage(
-        `To login with your ontrack account, click the 'login' button in the
-        top-left corner of the screen.`
-      ),
-      createChatBotMessage(
-        `If you don't have an ontrack account yet, you can create one using the
-        'Sign up' button instead.`
-      ),
-      createChatBotMessage(
-        `On smaller screens, the 'Login' and 'Sign up' buttons might be hidden
-        in the burger menu (three horizontal bars) in the top-left of the
-        screen instead.`
-      )
-    ]
-    messageArr.forEach(botMessage);
-    solvedQuery();
-  };
-  
-  const handleOther = () => {
-    const message = createChatBotMessage(
-      "Sorry, I didn't catch that. Would you mind rephrasing?"
-    );
-    botMessage(message);
-  }
-  
-  const solvedQuery = () => {
-    const message = createChatBotMessage('Has this solved your query?');
-    botMessage(message);
-  }
-  
-  const botMessage = (message) => {
-    setState((prev) => ({...prev, messages: [...prev.messages, message]}));
-  }
 
-  return (
-    <div>
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          actions: {
-            handleLogin,
-            handleOther,
-          },
-        });
-      })}
-    </div>
-  );
+    const handleLogin = () => {
+        const messageArr = [
+            createChatBotMessage(
+                `To login with your ontrack account, click the 'Login' button
+                in the top-left corner of the screen.`
+            ),
+            createChatBotMessage(
+                `If you don't have an ontrack account yet, you can create one
+                using the 'Sign up' button instead.`
+            ),
+            createChatBotMessage(
+                `On smaller screens, the 'Login' and 'Sign up' buttons might be
+                hidden in the burger menu (three horizontal bars) in the top-
+                left of the screen instead.`
+            )
+        ];
+        messageArr.forEach(botMessage);
+        solvedQuery();
+    };
+    
+    
+    const handleSignup = () => {
+        const messageArr = [
+            createChatBotMessage(
+                `To sign up for an ontrack account, click the 'Sign up' button
+                in the top-left corner of the screen. Enter your details and
+                click 'Sign up'. You will automatically be logged in.`
+            ),
+            createChatBotMessage(
+                `On smaller screens, the 'Login' and 'Sign up' buttons might be
+                hidden in the burger menu (three horizontal bars) in the top-
+                left of the screen instead.`
+            )
+        ];
+        messageArr.forEach(botMessage);
+        solvedQuery();
+    }
+    
+    
+    const handleOther = () => {
+        const message = createChatBotMessage(
+            "Sorry, I didn't catch that. Would you mind rephrasing?"
+        );
+        botMessage(message);
+    }
+    
+    
+    const solvedQuery = () => {
+        const message = createChatBotMessage('Has this solved your query?');
+        botMessage(message);
+    }
+    
+    
+    const botMessage = (message) => {
+        setState((prev) => ({...prev, messages: [...prev.messages, message]}));
+    }
+  
+    
+    return (
+        <div>
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, {
+                    actions: {
+                        handleLogin,
+                        handleSignup,
+                        handleOther
+                    },
+                });
+            })}
+        </div>
+    );
 };
 
 export default ActionProvider;
--- src/components/chatWindow/MessageParser.jsx
+++ src/components/chatWindow/MessageParser.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
 
 const MessageParser = ({ children, actions }) => {
-  const parse = (message) => {
-    if (message.toLowerCase().includes('login')) {
-      actions.handleLogin();
-    } else {
-      actions.handleOther();
-    }
-  };
+    const parse = (message) => {
+        if (message.toLowerCase().includes('login')) {
+            actions.handleLogin();
+        }
+        else if (message.toLowerCase().includes('sign') ||
+                 message.toLowerCase().includes('signup')) {
+            actions.handleSignup();
+        }
+        else {
+            actions.handleOther();
+        }
+    };
 
-  return (
-    <div>
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          parse: parse,
-          actions: {},
-        });
-      })}
-    </div>
-  );
+    return (
+        <div>
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, {
+                    parse: parse,
+                    actions: {},
+                });
+            })}
+        </div>
+    );
 };
 
 export default MessageParser;
